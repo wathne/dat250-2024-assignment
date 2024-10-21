318594138da915aa798107e2fe36a1262238a56e
LisaCabot <lisacabot@gmail.com>
2024-10-21 13:52:23 +0200

    Added checkt in inputs

--- social_insecurity/forms.py
+++ social_insecurity/forms.py
@@ -2,22 +2,9 @@
 
 This file is used to define all forms used in the application.
 It is imported by the social_insecurity package.
-
-Example:
-    from flask import Flask
-    from app.forms import LoginForm
-
-    app = Flask(__name__)
-
-    # Use the form
-    form = LoginForm()
-    if form.validate_on_submit() and form.login.submit.data:
-        username = form.username.data
 """
 
 from datetime import datetime
-from typing import cast
-
 from flask_wtf import FlaskForm
 from wtforms import (
     BooleanField,
@@ -29,48 +16,80 @@ from wtforms import (
     SubmitField,
     TextAreaField,
 )
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp
 
 # Defines all forms in the application, these will be instantiated by the template,
 # and the routes.py will read the values of the fields
 
-# TODO: Add validation, maybe use wtforms.validators??
-
-# TODO: There was some important security feature that wtforms provides, but I don't remember what; implement it
-
 
 class LoginForm(FlaskForm):
     """Provides the login form for the application."""
-
-    username = StringField(label="Username", render_kw={"placeholder": "Username"})
-    password = PasswordField(label="Password", render_kw={"placeholder": "Password"})
-    remember_me = BooleanField(
-        label="Remember me"
-    )  # TODO: It would be nice to have this feature implemented, probably by using cookies
+    
+    username = StringField(
+        label="Username", 
+        render_kw={"placeholder": "Username"}, 
+        validators=[DataRequired(), Length(min=3, max=15), 
+                    Regexp('^A-Za-z0-9!@#$%^&*.*$')] 
+    )
+    password = PasswordField(
+        label="Password", 
+        render_kw={"placeholder": "Password"}, 
+        validators=[DataRequired(), Length(min=5),
+                    Regexp('^A-Za-z0-9!@#$%^&*.*$')]
+    )
+    remember_me = BooleanField(label="Remember me")
     submit = SubmitField(label="Sign In")
 
 
 class RegisterForm(FlaskForm):
     """Provides the registration form for the application."""
 
-    first_name = StringField(label="First Name", render_kw={"placeholder": "First Name"})
-    last_name = StringField(label="Last Name", render_kw={"placeholder": "Last Name"})
-    username = StringField(label="Username", render_kw={"placeholder": "Username"})
-    password = PasswordField(label="Password", render_kw={"placeholder": "Password"})
-    confirm_password = PasswordField(label="Confirm Password", render_kw={"placeholder": "Confirm Password"})
+    first_name = StringField(
+        label="First Name", 
+        render_kw={"placeholder": "First Name"}, 
+        validators=[DataRequired(), Length(min=2, max=20), 
+                    Regexp('^[A-Za-z]*$')]
+    )
+    last_name = StringField(
+        label="Last Name", 
+        render_kw={"placeholder": "Last Name"}, 
+        validators=[DataRequired(), Length(min=2, max=20), 
+                    Regexp('^[A-Za-z]*$')]
+    )
+    username = StringField(
+        label="Username", 
+        render_kw={"placeholder": "Username"}, 
+        validators=[DataRequired(), Length(min=3, max=15), 
+                    Regexp('^[A-Za-z]*$')]
+    )
+    password = PasswordField(
+        label="Password", 
+        render_kw={"placeholder": "Password"}, 
+        validators=[DataRequired(), Length(min=5), 
+                    Regexp('^A-Za-z0-9!@#$%^&*.*$')]
+    )
+    confirm_password = PasswordField(
+        label="Confirm Password", 
+        render_kw={"placeholder": "Confirm Password"}, 
+        validators=[DataRequired(), EqualTo('password', message="Passwords must match.")]
+    )
     submit = SubmitField(label="Sign Up")
-
 
 class IndexForm(FlaskForm):
     """Provides the composite form for the index page."""
 
-    login = cast(LoginForm, FormField(LoginForm))
-    register = cast(RegisterForm, FormField(RegisterForm))
+    login = FormField(LoginForm)
+    register = FormField(RegisterForm)
 
 
 class PostForm(FlaskForm):
     """Provides the post form for the application."""
 
-    content = TextAreaField(label="New Post", render_kw={"placeholder": "What are you thinking about?"})
+    content = TextAreaField(
+        label="New Post", 
+        render_kw={"placeholder": "What are you thinking about?"}, 
+        validators=[DataRequired(), Length(max=1000), Regexp('^[A-Za-z]*$')]  
+    )
     image = FileField(label="Image")
     submit = SubmitField(label="Post")
 
@@ -78,24 +97,52 @@ class PostForm(FlaskForm):
 class CommentsForm(FlaskForm):
     """Provides the comment form for the application."""
 
-    comment = TextAreaField(label="New Comment", render_kw={"placeholder": "What do you have to say?"})
+    comment = TextAreaField(
+        label="New Comment", 
+        render_kw={"placeholder": "What do you have to say?"}, 
+        validators=[DataRequired(), Length(max=1000), Regexp('^[A-Za-z]*$')]  
+    )
     submit = SubmitField(label="Comment")
 
 
 class FriendsForm(FlaskForm):
     """Provides the friend form for the application."""
 
-    username = StringField(label="Friend's username", render_kw={"placeholder": "Username"})
+    username = StringField(
+        label="Friend's username", 
+        render_kw={"placeholder": "Username"}, 
+        validators=[DataRequired(), Length(min=3, max=20), Regexp('^[A-Za-z]*$')]
+    )
     submit = SubmitField(label="Add Friend")
 
 
 class ProfileForm(FlaskForm):
     """Provides the profile form for the application."""
 
-    education = StringField(label="Education", render_kw={"placeholder": "Highest education"})
-    employment = StringField(label="Employment", render_kw={"placeholder": "Current employment"})
-    music = StringField(label="Favorite song", render_kw={"placeholder": "Favorite song"})
-    movie = StringField(label="Favorite movie", render_kw={"placeholder": "Favorite movie"})
-    nationality = StringField(label="Nationality", render_kw={"placeholder": "Your nationality"})
+    education = StringField(
+        label="Education", 
+        render_kw={"placeholder": "Highest education"}, 
+        validators=[Length(max=100)] 
+    )
+    employment = StringField(
+        label="Employment", 
+        render_kw={"placeholder": "Current employment"}, 
+        validators=[Length(max=100)] 
+    )
+    music = StringField(
+        label="Favorite song", 
+        render_kw={"placeholder": "Favorite song"}, 
+        validators=[Length(max=100)] 
+    )
+    movie = StringField(
+        label="Favorite movie", 
+        render_kw={"placeholder": "Favorite movie"}, 
+        validators=[Length(max=100)]
+    )
+    nationality = StringField(
+        label="Nationality", 
+        render_kw={"placeholder": "Your nationality"}, 
+        validators=[Length(max=100)] 
+    )
     birthday = DateField(label="Birthday", default=datetime.now())
     submit = SubmitField(label="Update Profile")
